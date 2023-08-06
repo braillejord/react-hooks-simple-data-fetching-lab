77a0755ef7dcf8499b352d368e579e688abe076c
BreElle Wells <breellejordyn@gmail.com>
2023-08-06 10:09:18 -0600

    fetch dog data and set state

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [newImage, setNewImage] = useState()
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(r => r.json())
+            .then(data => setNewImage(data.message))
+    }, [])
+
+    if (!newImage) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
+    return (
+        <img src={newImage} alt={"A Random Dog"} />
+    )
+
+}
+
+export default App;
